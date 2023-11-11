1530893c782527a1e775ba3b4d4b3842def7dc02
Gustavo Poppi <gustavopoppi@hotmail.com>
2023-11-10 21:06:47 -0300

    [DEV] Create Schudeling repository and controller

---
+++ src/main/java/com/api/agendamento/controller/SchudelingController.java
@@ -0,0 +1,9 @@
+package com.api.agendamento.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("schudeling")
+public class SchudelingController {
+}
