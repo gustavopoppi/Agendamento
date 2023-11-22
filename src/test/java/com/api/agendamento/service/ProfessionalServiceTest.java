955bcb31d575ca990fa3694677e1b1cf5859e36a
Gustavo Poppi <gustavopoppi@hotmail.com>
2023-11-22 20:33:05 -0300

    [DEV] Create new class tests for Professional and Schudeling

---
+++ src/test/java/com/api/agendamento/service/ProfessionalServiceTest.java
@@ -0,0 +1,26 @@
+package com.api.agendamento.service;
+
+import org.junit.jupiter.api.Test;
+
+class ProfessionalServiceTest {
+
+    @Test
+    void getAllProfessionals() {
+    }
+
+    @Test
+    void getProfessionalById() {
+    }
+
+    @Test
+    void recordProfessional() {
+    }
+
+    @Test
+    void updateProfessional() {
+    }
+
+    @Test
+    void deleteProfessional() {
+    }
+}
---
+++ src/test/java/com/api/agendamento/service/SchudelingServiceTest.java
@@ -0,0 +1,26 @@
+package com.api.agendamento.service;
+
+import org.junit.jupiter.api.Test;
+
+class SchudelingServiceTest {
+
+    @Test
+    void getAllSchudelings() {
+    }
+
+    @Test
+    void getSchudelingById() {
+    }
+
+    @Test
+    void recordSchudeling() {
+    }
+
+    @Test
+    void updateSchudeling() {
+    }
+
+    @Test
+    void deleteSchudeling() {
+    }
+}
